753c7875448a2c429483ec56806e7439de77f7c1
chuan shi(haoyang gao) <ghy118432@alibaba-inc.com>
2017-03-17 10:18:08 +0800

    update

--- app/index.js
+++ app/index.js
@@ -1,6 +1,34 @@
-class H {
-  constructor() {
+const THREE = require('three');
 
-  }
-}
-console.log(new H());
+const init = () => {
+
+  // renderer
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize(400, 300);
+  renderer.setClearColor(0x000000);
+  document.body.appendChild(renderer.domElement);
+
+  // scene
+  const scene = new THREE.Scene();
+
+  // camera
+  // const camera = new THREE.PerspectiveCamera(45, 4 / 3, 1, 1000);
+  // camera.position.set(0, 0, 5);
+  // scene.add(camera);
+
+  var camera = new THREE.PerspectiveCamera(45, 400 / 300, 1, 10);
+  camera.position.set(0, 0, 5);
+  scene.add(camera);
+
+  // a cube in the scene
+  var cube = new THREE.Mesh(new THREE.CubeGeometry(1, 1, 1),
+        new THREE.MeshBasicMaterial({
+            color: 0xff0000,
+            wireframe: true
+        })
+);
+  scene.add(cube);
+  renderer.render(scene, camera);
+};
+
+init();
