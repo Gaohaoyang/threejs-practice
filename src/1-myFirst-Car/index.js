ed6f1dfc40026b6d54047899d43129b512cfe8c1
chuanshi <ghy118432@alibaba-inc.com>
2020-12-18 20:39:09 +0800

    before html plugin

---
+++ src/1-myFirst-Car/index.js
@@ -0,0 +1,14 @@
+console.log('------------------------------------')
+const a = 123
+console.log(a)
+console.log('------------------------------------')
+
+class C {
+  constructor(name) {
+    this.name = name
+    console.log('constructor')
+  }
+}
+
+const c = new C('Joe')
+console.log(c.name)
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,44 @@
-const path = require('path');
+const path = require('path')
+const glob = require('glob')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: './app/index.js',
+  entry: glob.sync('./src/**/index.js').reduce((acc, path) => {
+    const entry = path.replace('/index.js', '').replace('/src', '')
+    acc[entry] = path
+    return acc
+  }, {}),
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: './[name]/main.js',
+    path: path.resolve(__dirname, 'demo'),
   },
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader'
-      }, {
-        test: /\.scss$/,
-        use: [
-          {
-            loader: 'style-loader' // creates style nodes from JS strings
-          }, {
-            loader: 'css-loader' // translates CSS into CommonJS
-          }, {
-            loader: 'sass-loader' // compiles Sass to CSS
-          }
-        ]
-      }
-    ]
-  }
-};
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      // {
+      //   test: /\.scss$/,
+      //   use: [
+      //     {
+      //       loader: 'style-loader', // creates style nodes from JS strings
+      //     },
+      //     {
+      //       loader: 'css-loader', // translates CSS into CommonJS
+      //     },
+      //     {
+      //       loader: 'sass-loader', // compiles Sass to CSS
+      //     },
+      //   ],
+      // },
+    ],
+  },
+  // plugins: [new HtmlWebpackPlugin()],
+}
