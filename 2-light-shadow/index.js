6a7341c35f7f8fded1a0079c57f0a6aadc7fd97c
chuanshi <ghy118432@alibaba-inc.com>
2021-04-17 11:33:20 +0800

    Updates

---
+++ 2-light-shadow/index.js
@@ -0,0 +1,15 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is not neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/*!*************************************!*\
+  !*** ./src/2-light-shadow/index.js ***!
+  \*************************************/
+eval("console.log('------------------------------------');\nvar b = 12300000;\nconsole.log(b);\nconsole.log('------------------------------------');\n\n//# sourceURL=webpack://threejs-practice/./src/2-light-shadow/index.js?");
+/******/ })()
+;
